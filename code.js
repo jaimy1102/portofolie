be3253cce3545e8c39603b8c14e42e73fdcbed60
jaimy1102 <145451421+jaimy1102@users.noreply.github.com>
2024-10-21 14:34:09 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,30 @@
+function tekno() {
+    document.getElementById("box1").onclick = function () {
+        location.href = "teknologi.html";
+}
+}
+
+function program() {
+    document.getElementById("box2").onclick = function () {
+        location.href = "programering.html";
+}
+}
+
+function prod() {
+    document.getElementById("box3").onclick = function () {
+        location.href = "prod-hist.html";
+}
+}
+
+function home() {
+    document.getElementById("home").onclick = function ()  {
+        location.href = "index.html";
+    }
+}
+
+function spel() {
+    document.getElementById("spel").onclick = function ()  {
+        location.href = "/spelmagasin/spelmagasin.html";
+    }
+}
+
---
+++ sub day shitty game/game/code.js
@@ -0,0 +1,227 @@
+function start() {
+    
+    document.getElementById("startBox").style.display = "none";
+    document.getElementById("startHouse").style.display = "block";
+    document.getElementById("title").style.display = "none";
+    document.getElementById("enterHouse").style.display = "none";
+    document.getElementById("pizza").style.display = "none";
+    document.getElementById("walking").style.display = "none";
+    document.getElementById("backgroundImage").style.display = "none";
+    document.getElementById("vader").style.display = "none";
+    document.getElementById("fight").style.display = "none";
+    document.getElementById("punchf").style.display = "none";
+    document.getElementById("kickf").style.display = "none";
+    document.getElementById("gameover").style.display = "none";
+    document.getElementById("sandat").style.display = "none";
+}
+
+function enter() {
+    document.getElementById("startBox").style.display = "none";
+    document.getElementById("startHouse").style.display = "none";
+    document.getElementById("title").style.display = "none";
+    document.getElementById("enterHouse").style.display = "block";
+    document.getElementById("pizza").style.display = "none";
+    document.getElementById("walking").style.display = "none";
+    document.getElementById("backgroundImage").style.display = "none";
+    document.getElementById("vader").style.display = "none";
+    document.getElementById("fight").style.display = "none";
+    document.getElementById("punchf").style.display = "none";
+    document.getElementById("kickf").style.display = "none";
+    document.getElementById("gameover").style.display = "none";
+    document.getElementById("sandat").style.display = "none";
+}
+
+function leave() {
+      
+}
+
+function enterContinue() {
+    document.getElementById("startBox").style.display = "none";
+    document.getElementById("startHouse").style.display = "none";
+    document.getElementById("title").style.display = "none";
+    document.getElementById("enterHouse").style.display = "none";
+    document.getElementById("pizza").style.display = "block";
+    document.getElementById("walking").style.display = "none";
+    document.getElementById("backgroundImage").style.display = "none";
+    document.getElementById("vader").style.display = "none";
+    document.getElementById("fight").style.display = "none";
+    document.getElementById("punchf").style.display = "none";
+    document.getElementById("kickf").style.display = "none";
+    document.getElementById("gameover").style.display = "none";
+    document.getElementById("sandat").style.display = "none";
+}
+
+function droppizza() {
+    document.getElementById("startBox").style.display = "none";
+    document.getElementById("startHouse").style.display = "none";
+    document.getElementById("title").style.display = "none";
+    document.getElementById("enterHouse").style.display = "none";
+    document.getElementById("pizza").style.display = "none";
+    document.getElementById("walking").style.display = "block";
+    document.getElementById("backgroundImage").style.display = "none";
+    document.getElementById("vader").style.display = "none";
+    document.getElementById("fight").style.display = "none";
+    document.getElementById("punchf").style.display = "none";
+    document.getElementById("kickf").style.display = "none";
+    document.getElementById("gameover").style.display = "none";
+    document.getElementById("sandat").style.display = "none";
+}
+
+function eatpizza() {
+
+}
+
+function walkingContinue() {
+    document.getElementById("startBox").style.display = "none";
+    document.getElementById("startHouse").style.display = "none";
+    document.getElementById("title").style.display = "none";
+    document.getElementById("enterHouse").style.display = "none";
+    document.getElementById("pizza").style.display = "none";
+    document.getElementById("walking").style.display = "none";
+    document.getElementById("backgroundImage").style.display = "none";
+    document.getElementById("vader").style.display = "block";
+    document.getElementById("fight").style.display = "none";
+    document.getElementById("punchf").style.display = "none";
+    document.getElementById("kickf").style.display = "none";
+    document.getElementById("gameover").style.display = "none";
+    document.getElementById("sandat").style.display = "none";
+}
+
+function vaderflee() {
+    
+    
+}
+
+function vaderfight() {
+    document.getElementById("startBox").style.display = "none";
+    document.getElementById("startHouse").style.display = "none";
+    document.getElementById("title").style.display = "none";
+    document.getElementById("enterHouse").style.display = "none";
+    document.getElementById("pizza").style.display = "none";
+    document.getElementById("walking").style.display = "none";
+    document.getElementById("backgroundImage").style.display = "none";
+    document.getElementById("fight").style.display = "block";
+    document.getElementById("vader").style.display = "none";
+    document.getElementById("punchf").style.display = "none";
+    document.getElementById("kickf").style.display = "none";
+    document.getElementById("gameover").style.display = "none";
+    document.getElementById("sandat").style.display = "none";
+}
+
+function punch() {
+    document.getElementById("startBox").style.display = "none";
+    document.getElementById("startHouse").style.display = "none";
+    document.getElementById("title").style.display = "none";
+    document.getElementById("enterHouse").style.display = "none";
+    document.getElementById("pizza").style.display = "none";
+    document.getElementById("walking").style.display = "none";
+    document.getElementById("backgroundImage").style.display = "none";
+    document.getElementById("fight").style.display = "none";
+    document.getElementById("vader").style.display = "none";
+    document.getElementById("punchf").style.display = "block";
+    document.getElementById("kickf").style.display = "none";
+    document.getElementById("gameover").style.display = "none";
+    document.getElementById("sandat").style.display = "none";
+}
+
+function kick() {
+    document.getElementById("startBox").style.display = "none";
+    document.getElementById("startHouse").style.display = "none";
+    document.getElementById("title").style.display = "none";
+    document.getElementById("enterHouse").style.display = "none";
+    document.getElementById("pizza").style.display = "none";
+    document.getElementById("walking").style.display = "none";
+    document.getElementById("backgroundImage").style.display = "none";
+    document.getElementById("fight").style.display = "none";
+    document.getElementById("vader").style.display = "none";
+    document.getElementById("punchf").style.display = "none";
+    document.getElementById("kickf").style.display = "block";
+    document.getElementById("gameover").style.display = "none";
+    document.getElementById("sandat").style.display = "none";
+}
+
+function punchContinue() {
+    document.getElementById("startBox").style.display = "none";
+    document.getElementById("startHouse").style.display = "none";
+    document.getElementById("title").style.display = "none";
+    document.getElementById("enterHouse").style.display = "none";
+    document.getElementById("pizza").style.display = "none";
+    document.getElementById("walking").style.display = "none";
+    document.getElementById("backgroundImage").style.display = "none";
+    document.getElementById("fight").style.display = "none";
+    document.getElementById("vader").style.display = "none";
+    document.getElementById("punchf").style.display = "none";
+    document.getElementById("kickf").style.display = "none";
+    document.getElementById("gameover").style.display = "block";
+    document.getElementById("sandat").style.display = "none";
+
+}
+
+function retry() {
+    document.getElementById("startBox").style.display = "block";
+    document.getElementById("startHouse").style.display = "none";
+    document.getElementById("title").style.display = "block";
+    document.getElementById("enterHouse").style.display = "none";
+    document.getElementById("pizza").style.display = "none";
+    document.getElementById("walking").style.display = "none";
+    document.getElementById("backgroundImage").style.display = "block";
+    document.getElementById("fight").style.display = "none";
+    document.getElementById("vader").style.display = "none";
+    document.getElementById("punchf").style.display = "none";
+    document.getElementById("kickf").style.display = "none";
+    document.getElementById("gameover").style.display = "none";
+    document.getElementById("gameover").style.display = "none";
+    document.getElementById("sandat").style.display = "none";
+}
+
+
+function sand() {
+    document.getElementById("startBox").style.display = "none";
+    document.getElementById("startHouse").style.display = "none";
+    document.getElementById("title").style.display = "none";
+    document.getElementById("enterHouse").style.display = "none";
+    document.getElementById("pizza").style.display = "none";
+    document.getElementById("walking").style.display = "none";
+    document.getElementById("backgroundImage").style.display = "none";
+    document.getElementById("fight").style.display = "none";
+    document.getElementById("vader").style.display = "none";
+    document.getElementById("punchf").style.display = "none";
+    document.getElementById("kickf").style.display = "none";
+    document.getElementById("gameover").style.display = "none";
+    document.getElementById("sandat").style.display = "none";
+    document.getElementById("youwin").style.display = "block";
+}
+
+function kickContinue() {
+    document.getElementById("startBox").style.display = "none";
+    document.getElementById("startHouse").style.display = "none";
+    document.getElementById("title").style.display = "none";
+    document.getElementById("enterHouse").style.display = "none";
+    document.getElementById("pizza").style.display = "none";
+    document.getElementById("walking").style.display = "none";
+    document.getElementById("backgroundImage").style.display = "none";
+    document.getElementById("fight").style.display = "none";
+    document.getElementById("vader").style.display = "none";
+    document.getElementById("punchf").style.display = "none";
+    document.getElementById("kickf").style.display = "none";
+    document.getElementById("gameover").style.display = "none";
+    document.getElementById("gameover").style.display = "none";
+    document.getElementById("sandat").style.display = "block";
+
+}
+
+function win() {
+    document.getElementById("startBox").style.display = "block";
+    document.getElementById("startHouse").style.display = "none";
+    document.getElementById("title").style.display = "block";
+    document.getElementById("enterHouse").style.display = "none";
+    document.getElementById("pizza").style.display = "none";
+    document.getElementById("walking").style.display = "none";
+    document.getElementById("backgroundImage").style.display = "block";
+    document.getElementById("fight").style.display = "none";
+    document.getElementById("vader").style.display = "none";
+    document.getElementById("punchf").style.display = "none";
+    document.getElementById("kickf").style.display = "none";
+    document.getElementById("gameover").style.display = "none";
+    document.getElementById("sandat").style.display = "none";
+}
